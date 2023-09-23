ae6bd035117ffdba1c7efc052e41f97229ca0752
kkh5535 <kkh5535@naver.com>
2023-09-23 15:40:34 +0900

    예외처리

--- src/main/java/com/nhnacademy/Bingo.java
+++ src/main/java/com/nhnacademy/Bingo.java
@@ -1,6 +1,7 @@
 package com.nhnacademy;
 
 import com.nhnacademy.exceptions.NotChangedException;
+import com.nhnacademy.exceptions.NotEnoughException;
 import com.nhnacademy.exceptions.NotInBoundsException;
 
 public class Bingo {
@@ -13,20 +14,33 @@ public class Bingo {
     }
 
     // 빙고판 세팅
+    // NumberFormatException - 숫자가 아닙니다.
+    // NotInBoundsException - 1부터 99까지의 정수를 입력해주세요.
+    // NotEnoughException - 숫자가 모자랍니다.
     public void setting(String a) {
         String[] numbers = a.split(",");
+        if (numbers.length > array.length * array.length) {
+            throw new NotEnoughException();
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
+                int x = Integer.parseInt(numbers[array.length * i + j].trim());
+                if (x > 100 || x <= 0) {
+                    throw new NotInBoundsException();
+                }
                 array[i][j] = numbers[array.length * i + j].trim();
             }
         }
     }
 
     // 나의 차례일 때의 처리
+    // NumberFormatException
+    // NotInBoundsException
+    // NotChangedException - 숫자가 빙고판에 없습니다.
     public void inputO(String a) {
         boolean isChanged = false;
         int x = Integer.parseInt(a);
-        if (x > 100 || x < 1) {
+        if (x > 100 || x <= 0) {
             throw new NotInBoundsException();
         }
         for (int i = 0; i < array.length; i++) {
@@ -52,4 +66,17 @@ public class Bingo {
             }
         }
     }
+
+    // 빙고판 출력
+    public String toString() {
+        String string = "";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                string += array[i][j] + "/t";
+            }
+            string += "\n";
+        }
+        return string;
+    }
+
 }
