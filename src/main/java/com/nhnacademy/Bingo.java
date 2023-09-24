cab36dd195eeefe180f94e399788ae049d4ce84d
kkh5535 <kkh5535@naver.com>
2023-09-24 15:54:56 +0900

    제출

--- src/main/java/com/nhnacademy/Bingo.java
+++ src/main/java/com/nhnacademy/Bingo.java
@@ -40,7 +40,7 @@ public class Bingo extends Thread {
         this.size = 5;
         changed = new boolean[5][5];
         board = new String[5][5];
-        roomSize = 10;
+        roomSize = 2;
     }
 
     public void setSize(int n) {
@@ -100,11 +100,11 @@ public class Bingo extends Thread {
                     if (serverList.size() == 1) {
                         send("빙고판의 크기를 지정해 주세요");
                         size = Integer.parseInt(in.readLine());
-                        send("대전 방의 크기를 지정해 주세요");
-                        roomSize = Integer.parseInt(in.readLine());
-                        for (Bingo bingo : serverList) {
-                            bingo.roomSize = roomSize;
-                        }
+                        // send("대전 방의 크기를 지정해 주세요");
+                        // roomSize = Integer.parseInt(in.readLine());
+                        // for (Bingo bingo : serverList) {
+                        // bingo.roomSize = roomSize;
+                        // }
                     }
 
                     if (serverList.size() < serverList.get(0).roomSize) {
@@ -116,12 +116,12 @@ public class Bingo extends Thread {
                         serverList.notifyAll();
                     }
 
-                    if (serverList.indexOf(this) >= serverList.get(0).roomSize) {
-                        send(getUserName());
-                        send("방이 꽉 찼습니다.");
-                        socket.close();
-                        serverList.remove(this);
-                    }
+                    // if (serverList.indexOf(this) >= serverList.get(0).roomSize) {
+                    // send(getUserName());
+                    // send("방이 꽉 찼습니다.");
+                    // socket.close();
+                    // serverList.remove(this);
+                    // }
                 } catch (NumberFormatException e) {
                     send("숫자를 입력해주세요");
                 }
